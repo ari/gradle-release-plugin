b43125bae7678c8adc14909a0647a36ae52e4adf
Ari Maniatis <ari@ish.com.au>
2014-08-03 17:04:34 +1000

    Use publicly visible svn URLs for testing rather than private urls only visible to the developer.

--- src/test/groovy/au/com/ish/gradle/SVNServiceTest.groovy
+++ src/test/groovy/au/com/ish/gradle/SVNServiceTest.groovy
@@ -32,37 +32,37 @@ class SvnServiceTest {
     @Test
     public void testOnTag() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/tags/admin/10.0")
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/tags/1.8.0")
         assert service.onTag() == true
-        assert service.getBranchName() == "10.0"
+        assert service.getBranchName() == "1.8.0"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/tags/10.0")
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/tags/1.8.0/doc")
         assert service.onTag() == true
-        assert service.getBranchName() == "10.0"
+        assert service.getBranchName() == "doc"
     }
 
     // tests parsing the url with 'branches'
     @Test
     public void testOnBranch() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/branches/admin/stable")
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/branches/1.7.x/doc")
         assert service.onTag() == false
-        assert service.getBranchName() == "stable"
+        assert service.getBranchName() == "doc"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/branches/stable")
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/branches/1.7.x")
         assert service.onTag() == false
-        assert service.getBranchName() == "stable"
+        assert service.getBranchName() == "1.7.x"
     }
 
     // tests parsing the url with 'trunk'
     @Test
     public void testOnTrunk() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/trunk/admin")
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/trunk/doc")
         assert service.onTag() == false
         assert service.getBranchName() == "trunk"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/trunk")
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/trunk")
         assert service.onTag() == false
         assert service.getBranchName() == "trunk"
     }
@@ -71,33 +71,33 @@ class SvnServiceTest {
     @Test
     public void testRootURLonTags() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/tags/admin/10.0")
-        assert service.getSvnRootURL() == "http://svn.ish.com.au/ish/willow"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/tags/1.8.0/doc/")
+        assert service.getSvnRootURL() == "http://svn.apache.org/repos/asf/subversion"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/tags/10.0")
-        assert service.getSvnRootURL() == "http://svn.ish.com.au/ish/angel"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/tags/1.8.0")
+        assert service.getSvnRootURL() == "http://svn.apache.org/repos/asf/subversion"
     }
 
     // tests parsing the root url with 'branches'
     @Test
     public void testRootURLonBranches() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/branches/admin/stable")
-        assert service.getSvnRootURL() == "http://svn.ish.com.au/ish/willow"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/branches/1.7.x/doc")
+        assert service.getSvnRootURL() == "http://svn.apache.org/repos/asf/subversion"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/branches/stable")
-        assert service.getSvnRootURL() == "http://svn.ish.com.au/ish/angel"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/branches/1.7.x")
+        assert service.getSvnRootURL() == "http://svn.apache.org/repos/asf/subversion"
     }
 
     // tests parsing the root url with 'trunk'
     @Test
     public void testRootURLonTrunk() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/trunk/admin")
-        assert service.getSvnRootURL() == "http://svn.ish.com.au/ish/willow"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/trunk/doc")
+        assert service.getSvnRootURL() == "http://svn.apache.org/repos/asf/subversion"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/trunk")
-        assert service.getSvnRootURL() == "http://svn.ish.com.au/ish/angel"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/trunk")
+        assert service.getSvnRootURL() == "http://svn.apache.org/repos/asf/subversion"
     }
 
 
@@ -105,32 +105,32 @@ class SvnServiceTest {
     @Test
     public void testCreateTagsUrlonTrunk() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/trunk/admin")
-        assert service.createTagsUrl("10.0").toDecodedString() == "http://svn.ish.com.au/ish/willow/tags/admin/10.0"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/trunk/doc")
+        assert service.createTagsUrl("10.0").toDecodedString() == "http://svn.apache.org/repos/asf/subversion/tags/doc/10.0"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/trunk")
-         assert service.createTagsUrl("10.0").toDecodedString() == "http://svn.ish.com.au/ish/angel/tags/10.0"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/trunk")
+         assert service.createTagsUrl("10.0").toDecodedString() == "http://svn.apache.org/repos/asf/subversion/tags/10.0"
     }
 
     // tests creating tag url from url with 'branches'
     @Test
     public void testCreateTagsUrlonBranch() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/branches/admin/stable")
-        assert service.createTagsUrl("10.0").toDecodedString() == "http://svn.ish.com.au/ish/willow/tags/admin/10.0"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/branches/1.7.x/doc")
+        assert service.createTagsUrl("10.0").toDecodedString() == "http://svn.apache.org/repos/asf/subversion/tags/1.7.x/10.0"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/branches/stable")
-         assert service.createTagsUrl("10.0").toDecodedString() == "http://svn.ish.com.au/ish/angel/tags/10.0"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/branches/1.7.x")
+         assert service.createTagsUrl("10.0").toDecodedString() == "http://svn.apache.org/repos/asf/subversion/tags/10.0"
     }
 
     // tests creating tag url from url with 'tags'
     @Test
     public void testCreateTagsUrlonTags() {
         TestSvnService service = new TestSvnService()
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/willow/tags/admin/10.0")
-        assert service.createTagsUrl("10.0.1").toDecodedString() == "http://svn.ish.com.au/ish/willow/tags/admin/10.0.1"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/tags/1.8.0/doc")
+        assert service.createTagsUrl("doc2").toDecodedString() == "http://svn.apache.org/repos/asf/subversion/tags/1.8.0/doc2"
 
-        service.setSCMRemoteURL("http://svn.ish.com.au/ish/angel/tags/10.0")
-         assert service.createTagsUrl("10.0.1").toDecodedString() == "http://svn.ish.com.au/ish/angel/tags/10.0.1"
+        service.setSCMRemoteURL("http://svn.apache.org/repos/asf/subversion/tags/1.8.0")
+         assert service.createTagsUrl("1.8.1").toDecodedString() == "http://svn.apache.org/repos/asf/subversion/tags/1.8.1"
     }
 }
